8b75d2905b57b23c7b0d472e93294b1c1739a705
Jarryd Cheso <jarryd.cheso@gmail.com>
2022-01-15 14:33:50 +0000

    fix: add to allowed hosts

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", default="foo")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "epic-site-backend.herokuapp.com"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "epc-site-backend.herokuapp.com"]
 
 
 # Application definition
