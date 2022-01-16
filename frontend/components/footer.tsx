13e19ba16edb25426f329ac2d116df0daeab5102
jcheso <jcheso@users.noreply.github.com>
2022-01-16 12:50:20 +0000

    Prettified Code!

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 
 export default function Footer() {
-    return (
-        <div className="w-full h-10 text-gray-500 fixed bottom-0 flex justify-center items-center">
-            <span className="">Copyright footer | Links to github</span>
-        </div>
-
-    )
+  return (
+    <div className="w-full h-10 text-gray-500 fixed bottom-0 flex justify-center items-center">
+      <span className="">Copyright footer | Links to github</span>
+    </div>
+  );
 }
