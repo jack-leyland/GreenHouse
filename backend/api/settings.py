c293c662e81a662f8c055cf40a0f01a6f1e1a2be
Jarryd Cheso <jarryd.cheso@gmail.com>
2022-01-16 13:45:47 +0000

    set debug true

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", default="foo")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.environ.get("DEBUG", default=0))
+DEBUG = int(os.environ.get("DEBUG", default=1))
 
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "epc-site-backend.herokuapp.com"]
