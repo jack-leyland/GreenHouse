6fe175704a66c4da13d9e365a1cf4f444c36eb63
Joshua Cheng <joshcheng4@gmail.com>
2022-01-18 13:58:10 +0000

    add search by LMK in graphene

--- backend/api/schema.py
+++ backend/api/schema.py
@@ -11,15 +11,23 @@ payload = {}
 
 class Query(ObjectType):
     address = String(name=String(default_value="N/A"))
+    epc = String(name=String(default_value="N/A"))
 
     def resolve_address(root, info, name):
-        input_postcode = name
-        url = f"https://epc.opendatacommunities.org/api/v1/domestic/search?postcode={input_postcode}"
+        url = f"https://epc.opendatacommunities.org/api/v1/domestic/search?postcode={name}"
         response = requests.request("GET", url, headers=headers, data=payload)
         responseJSON = response.json()["rows"]
 
-        data = [item["address"] for item in responseJSON]
+        data = [{"address" : item["address"], "lmk-key" : item["lmk-key"]} for item in responseJSON]
         return json.dumps(data)
 
+    def resolve_epc(root, info, name):
+        url = f"https://epc.opendatacommunities.org/api/v1/domestic/certificate/{name}"
+        response = requests.request("GET", url, headers=headers, data=payload)
+        responseJSON = response.json()
+
+        if (not responseJSON): return {"Error": "Invalid LMK key" }
+
+        return responseJSON["rows"][0]
 
 schema = Schema(query=Query)
