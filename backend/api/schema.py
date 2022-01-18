9d3f7e1d13a45b1dfb0e2ec29d06968dab117a7b
Joshua Cheng <joshcheng4@gmail.com>
2022-01-18 13:29:44 +0000

    add postcode query in graphene

--- backend/api/schema.py
+++ backend/api/schema.py
@@ -1,7 +1,23 @@
-import graphene
+from graphene import ObjectType, String, Schema
+import requests
+import json
 
-class Query(graphene.ObjectType):
-    hello = graphene.String(default_value="Hi!")
+class Query(ObjectType):
+    postcode = String(name=String(default_value="N/A"))
+
+    def resolve_postcode(root, info, name):
+        input_postcode = "SW109EF"
+        url = f"https://epc.opendatacommunities.org/api/v1/domestic/search?postcode={input_postcode}"
+        headers = {
+            'Accept': 'application/json',
+            'Authorization': 'Basic amFycnlkLmNoZXNvQGdtYWlsLmNvbTpkMzEwOWRmYmI4ZDI2OWZiZDkxMjFlY2U0NGMxNjY2NTA1MDBiM2Jm'
+        }
+        payload = {}
+        response = requests.request("GET", url, headers=headers, data=payload)
+        responseJSON = response.json()["rows"]
+
+        data = [item["address"] for item in responseJSON]
+        return json.dumps(data)
 
 
-schema = graphene.Schema(query=Query)
+schema = Schema(query=Query)
