24538094519a4bbf6bff4b8eaf860026e76483bc
Will Thomson <will.thomson4@gmail.com>
2022-01-20 18:19:15 +0000

    Footer fitting

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="w-full h-10 text-gray-500 fixed bottom-0 flex justify-center items-center">
+    <div className="relative w-full h-10 text-gray-500 fixed bottom-0 flex justify-center items-center">
       <span className="">Copyright footer | Links to github</span>
     </div>
   );
--- frontend/components/sidebar.tsx
+++ frontend/components/sidebar.tsx
@@ -4,7 +4,7 @@ import {BsFillPinMapFill, BsWrench, BsFillHouseFill, BsFillLightbulbFill} from "
 export default function Sidebar() {
     return(
     <div className="md:flex flex-col md:flex-row md:min-h-screen">
-        <div className="shadow-md flex flex-col w-full md:w-72 text-gray-700 bg-white dark-mode:text-gray-200 dark-mode:bg-gray-800 flex-shrink-0">
+        <div className="flex flex-col w-full md:w-72 text-gray-700 bg-white dark-mode:text-gray-200 dark-mode:bg-gray-800 flex-shrink-0">
             <div className="flex-shrink-0 px-8 py-4 flex flex-row items-center justify-between">
             <nav  className="flex-grow md:block px-4 pb-4 md:pb-0 md:overflow-y-auto">  
             <span className="block pt-2 pb-6 mt-2 text-4xl font-bold text-green-400">GreenHouse</span>   
