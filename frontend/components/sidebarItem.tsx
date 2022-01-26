951aea06720cd28c474fad52090c3e353bb5afbb
Will Thomson <will.thomson4@gmail.com>
2022-01-26 20:03:39 +0000

    Color tweaks

--- frontend/components/sidebarItem.tsx
+++ frontend/components/sidebarItem.tsx
@@ -7,6 +7,6 @@ interface props {
 
 export default function SidebarItem({icon, label} : props) {
     return(
-        <a className="hover:cursor-pointer flex justify-start gap-3 px-2 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg hover:bg-lightGreen focus:bg-gray-200 focus:outline-none focus:shadow-outline">{icon}{label}</a>
+        <a className="hover:cursor-pointer flex justify-start gap-3 px-2 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg hover:bg-emerald focus:bg-gray-200 focus:outline-none focus:shadow-outline">{icon}{label}</a>
     )
 }
