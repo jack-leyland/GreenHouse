b668d7205c1fb6d055fa72886d1d0fb44b90a9d9
jack-leyland <jackjleyland@gmail.com>
2022-02-11 10:08:47 +0000

    overview views progress

--- frontend/components/card.tsx
+++ frontend/components/card.tsx
@@ -1,21 +1,34 @@
-import React from "react";
-import {Dispatch, SetStateAction} from "react";
+import React from 'react';
+import { Dispatch, SetStateAction } from 'react';
 
 interface props {
   children?: React.ReactChild | React.ReactChildren | never[];
   style: string;
-  onHover?:  Dispatch<SetStateAction<boolean>>;
+  letterColor?: string;
+  disableHoverAnimation: boolean;
+  onHover?: Dispatch<SetStateAction<boolean>>;
+  showShadow: boolean;
 }
 
-export default function Card({ children, style, onHover }: props) {
+export default function Card({
+  children,
+  style,
+  onHover,
+  disableHoverAnimation,
+  showShadow,
+}: props) {
+  const hover = ' hover:scale-105 ';
+  const shadowAndCursor = ' shadow-lg hover:cursor-pointer ';
   const cardStyle =
-    "animate-fade hover:scale-105 hover:cursor-pointer shadow-lg rounded-lg w-full h-full " +
-    style;
+    'animate-fade rounded-lg w-full h-full ' +
+    style +
+    (disableHoverAnimation ? '' : hover) +
+    (showShadow ? shadowAndCursor : '');
   return (
-    <div 
+    <div
       className={cardStyle}
-      onMouseEnter={() => onHover ? onHover(true) : null}
-      onMouseLeave={() => onHover ? onHover(false) : null}
+      onMouseEnter={() => (onHover ? onHover(true) : null)}
+      onMouseLeave={() => (onHover ? onHover(false) : null)}
     >
       <div className="p-3 text-xs h-full w-full">{children}</div>
     </div>
