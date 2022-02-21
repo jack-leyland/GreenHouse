7befb26da8be30c666d00303c24df80639164177
Will Thomson <will.thomson4@gmail.com>
2022-02-21 13:46:20 +0000

    EPC color types

--- frontend/types.ts
+++ frontend/types.ts
@@ -250,11 +250,11 @@ export interface epcCertificateResponse extends Object {
   };
 
   export const epcIndexDictionary: EPCDictionary<number> = {
-    A: 1,
-    B: 2,
-    C: 3,
-    D: 4,
-    E: 5,
-    F: 6,
-    G: 7,
+    A: 2,
+    B: 3,
+    C: 4,
+    D: 5,
+    E: 6,
+    F: 7,
+    G: 8,
   };
