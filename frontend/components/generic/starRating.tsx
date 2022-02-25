06fa10cd05408855a386a6a96196015fa9ffad46
jcheso <jcheso@users.noreply.github.com>
2022-02-25 12:26:01 +0000

    Prettified Code!

--- frontend/components/generic/starRating.tsx
+++ frontend/components/generic/starRating.tsx
@@ -1,32 +1,29 @@
-import React from 'react';
+import React from "react";
 const StarRatings = require("react-star-ratings").default;
-import {AiOutlineStar} from 'react-icons/ai';
+import { AiOutlineStar } from "react-icons/ai";
 
 interface props {
-    title: string;
-    rating?: number;
-    areaAverage?: number;
+  title: string;
+  rating?: number;
+  areaAverage?: number;
 }
 
-export default function StarRating({
-    title,
-    rating,
-    areaAverage
-}: props) {
-  
+export default function StarRating({ title, rating, areaAverage }: props) {
   return (
-    <div className='py-2 flex items-center gap-6'>
-    <div className="flex items-center">
-      <b className="pr-2">{title}:</b>
+    <div className="py-2 flex items-center gap-6">
+      <div className="flex items-center">
+        <b className="pr-2">{title}:</b>
         <StarRatings
           rating={rating ? rating : 0}
-          starRatedColor= {rating ? rating > 2 ? "green" : "red" : "gray"}
+          starRatedColor={rating ? (rating > 2 ? "green" : "red") : "gray"}
           numberOfStars={5}
-          name='rating'
+          name="rating"
           starDimension="15px"
         />
+      </div>
+      <div className="text-sm flex items-center justify-center italic">
+        (Area average: {areaAverage} <AiOutlineStar />)
+      </div>
     </div>
-      <div className="text-sm flex items-center justify-center italic">(Area average: {areaAverage} <AiOutlineStar/>)</div>
-  </div>
   );
 }
