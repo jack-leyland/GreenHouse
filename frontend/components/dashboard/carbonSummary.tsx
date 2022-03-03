b8f13bc5c5b85c8165ee3a2b74b6c4921ca0f1f8
Will Thomson <will.thomson4@gmail.com>
2022-03-03 19:57:08 +0000

    More detailed help modals

--- frontend/components/dashboard/carbonSummary.tsx
+++ frontend/components/dashboard/carbonSummary.tsx
@@ -50,18 +50,18 @@ export default function CarbonSummary({ data, analytics, setModalHandler }: prop
             <div className="px-4 py-1">
               <div className="border border-x-0 border-t-0">
                 <div className="py-1 text-sm">
-                  Your current CO<sub>2</sub> production is <b>{data.co2EmissionsCurrent}</b> Kg per year. 
+                  Your current CO<sub>2</sub> production is <b>{data.co2EmissionsCurrent}</b> tonnes per year. 
                 </div>
                 <div className="py-1 text-sm">
-                  Your potential CO<sub>2</sub> production is <b>{data.co2EmissionsPotential}{" "}</b> Kg per year.
+                  Your potential CO<sub>2</sub> production is <b>{data.co2EmissionsPotential}{" "}</b> tonnes per year.
                 </div>
-                <div className="pt-2 pb-3 italic text-blue-500 inline-flex items-center md:mb-2 lg:mb-0 hover:cursor-pointer">What does this mean <AiOutlineArrowRight/> </div>
+                <div onClick={()=>setModalHandler("carbonProduction")} className="pt-2 pb-3 italic text-blue-500 inline-flex items-center md:mb-2 lg:mb-0 hover:cursor-pointer">What does this mean <AiOutlineArrowRight/> </div>
               </div>
               <div className="pt-2 text-sm">
                 <strong>How you compare:</strong>
               </div>
               <div className="py-1 text-sm">
-                The average for CO<sub>2</sub> production for your area is <b>{analytics.meanCurrentCo2Consumption}</b> Kg per year.
+                The average for CO<sub>2</sub> production for your area is <b>{analytics.meanCurrentCo2Consumption}</b> tonnes per year.
               </div>
             </div>
           </div>
--- frontend/components/dashboard/helpModal.tsx
+++ frontend/components/dashboard/helpModal.tsx
@@ -8,48 +8,72 @@ export default function HelpModal({ type }: props) {
   switch(type) {
     case "epcChart":
         return (
-            <div className="w-full flex flex-col">
+            <div className="w-full flex flex-col p-2">
+                <div className="font-bold text-lg pt-2 mb-2 w-full border border-x-0 border-t-0">EPC Ratings</div>
                 <div className="py-1">
                     Properties are given a Standard Assessment Procedure (SAP) score from 0-100 based on their energy efficiency. 
-                    For easier readability, these scores are divided into ratings A-G (Higher scores e.g. those between 100-80, 
-                    which are rated A, are more energy efficienct, than lower scores e.g. 0-20, which are rated G).
+                    These scores broken down into bands from A-G (for more info and to see the boundaries <a className="text-blue-500" href="https://www.edfenergy.com/energy-efficiency/how-improve-your-epc-rating">click here</a>). 
                 </div>
                 <div className="py-1">
-                    SAP scores take into account a number of different factors including, but not limited to,
+                    SAP scores take into account a number of different factors including, but not limited to:
                     construction materials, heating systems, insulation, lighting, ventilation and air leakage. To see more information about 
-                    the SAP procedure see <a href="https://www.gov.uk/guidance/standard-assessment-procedure">here</a>
+                    the SAP calculation see <a className="text-blue-500" href="https://www.gov.uk/guidance/standard-assessment-procedure">here</a>.
                 </div>
             </div>
         )
     case "currentRating":
         return (
             <div className="w-full flex flex-col">
-                <div>
-                    Your current rating is based on the SAP score your house was given in it&apos;s last inspection (view 
-                    top of page for more information about your last EPC inspection).
+                <div className="font-bold text-lg pt-2 mb-2 w-full border border-x-0 border-t-0">Current Rating</div>
+                <div className="py-1">
+                    Your current rating is based on the SAP score your house was given in it&apos;s last inspection (See 
+                    the <i>More Info</i> button at the top of this page!)
                 </div>
             </div>
         )
     case "potentialRating":
         return (
             <div className="w-full flex flex-col">
-                <div>
+                <div className="font-bold text-lg pt-2 mb-2 w-full border border-x-0 border-t-0">Potential Rating</div>
+                <div className="py-1">
                     Your potential rating is a projection of the SAP score you could achieve if you make all the 
-                    suggested improvements to your house. To see more about your potential improvements, see our 
-                    <a href={`${window.location.href}/recomendations`}>recomendations page</a>!
+                    suggested improvements to your house. To see more about your potential improvements, see our <a className="text-blue-500" href="https://epc-site-frontend.vercel.app/recommendations">recommendations</a> page!
                 </div>
             </div>
         )
     case "costs":
         return (
             <div className="w-full flex flex-col">
-                <div>
+                <div className="font-bold text-lg pt-2 mb-2 w-full border border-x-0 border-t-0">Current Costs</div>
+                <div className="py-1">
                     Current costs are estimations based on the current efficiency of your house 
                     features and current energy prices (for more accurate cost estimations we advise using a smart meter!).
                 </div>
-                <div>
+                <div className="py-1">
                     Potential costs are projections of what your costs could be if you make all of the recommended improvements. 
-                    To check out these improvements see our <a href={`${window.location.href}/recomendations`}>recommendations page</a>!
+                    To check out these improvements see our <a className="text-blue-500" href="https://epc-site-frontend.vercel.app/recommendations">recommendations</a> page!
+                </div>
+            </div>
+        )
+    case "featureEfficiency":
+        return (
+            <div className="w-full flex flex-col">
+                <div className="font-bold text-lg pt-2 mb-2 w-full border border-x-0 border-t-0">Feature Efficiency</div>
+                <div className="py-1">
+                    All features in your house are given an environmental efficiency rating from 0-5 (5 being best, 0 being worst). 
+                    Features which are less environmentally friendly are given a lower rating. Click on
+                    the house to see more about each feature!
+                </div>
+            </div>
+        )
+
+    case "carbonProduction":
+        return (
+            <div className="w-full flex flex-col">
+                <div className="font-bold text-lg pt-2 mb-2 w-full border border-x-0 border-t-0">Carbon Production</div>
+                <div className="py-1">
+                    This is the estimated CO<sub>2</sub> production for your house in tonnes per year. This estimate is drawn from
+                    the estimated amount of energy your house uses for heating, lighting and hot water.
                 </div>
             </div>
         )
