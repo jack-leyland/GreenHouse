6f5ccc7e63062a55793b640a99a3e52d329fdecc
jack-leyland <jackjleyland@gmail.com>
2022-03-05 12:36:52 +0000

    add search to addressModal

--- frontend/components/landing/addressModal.tsx
+++ frontend/components/landing/addressModal.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import BackButton from '../../assets/arrow-left.svg';
 import Lottie from 'react-lottie-player';
 import loadingJson from '../../assets/animations/animation/loading.json';
+import SearchIcon from '../../assets/search-icon.svg';
 
 type AddressObject = {
   address: string;
@@ -26,6 +27,9 @@ export default function AddressList({
   const [loading, setLoading] = useState<boolean>(isLoading);
   const [err, setErr] = useState<boolean>(isError);
   const [addressData, setAddressData] = useState<Array<AddressObject>>([]);
+  const [filteredData, setFilteredData] = useState<Array<AddressObject>>([]);
+  const [showSearchBar, setShowSearchBar] = useState<Boolean>(false);
+  const [searchBoxText, setSearchBoxText] = useState<string>('');
 
   useEffect(() => {
     setLoading(isLoading);
@@ -38,8 +42,28 @@ export default function AddressList({
   useEffect(() => {
     if (data) {
       setAddressData(data);
+      setFilteredData(data);
     }
   }, [data]);
+
+  const handleSearchChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    setSearchBoxText(event.target.value);
+  };
+
+  useEffect(() => {
+    if (searchBoxText == '') {
+      setFilteredData(addressData);
+      return;
+    }
+    let filtered = addressData.filter((obj) => {
+      let stripped_address = obj.address.toLowerCase().replace(/ /g, '');
+      let stripped_search = searchBoxText.toLowerCase().replace(/ /g, '');
+      return stripped_address.includes(stripped_search);
+    });
+    setFilteredData(filtered);
+  }, [searchBoxText]);
 
   return (
     <div className="h-[50vh] w-[35vw] max-w-[450px] min-w-[250px] rounded-default border-lightGrey bg-lightGrey drop-shadow-md">
@@ -50,7 +74,24 @@ export default function AddressList({
         />
         Select your address
       </div>
-      <div className=" h-[calc(100%-45px)] overflow-y-scroll scrollbar-track-lightGrey scrollbar-thumb-rounde">
+      <SearchIcon
+        className="h-[35px] w-[35px] cursor-pointer stroke-2 stroke-black absolute right-[10px] top-[5px] transistion-all duration-100 hover:h-[40px] hover:w-[40px]"
+        onClick={() => setShowSearchBar(!showSearchBar)}
+      />
+      {showSearchBar ? (
+        <input
+          className="h-[45px] w-[100%] pl-[10px] font-logoFont border-b-2 text-gray-800 transistion-all duration-100 outline-none"
+          placeholder="Search for your address"
+          value={searchBoxText}
+          onChange={handleSearchChange}
+        ></input>
+      ) : null}
+      <div
+        className={
+          'h-[calc(100%-45px)] overflow-y-scroll scrollbar-track-lightGrey scrollbar-thumb-rounded ' +
+          (showSearchBar ? 'h-[calc(100%-90px)]' : 'h-[calc(100%-45px)]')
+        }
+      >
         {loading ? (
           <div className="w-[100%] h-[100%] flex flex-col text-gray-800 items-center justify-center">
             Loading Addresses...
@@ -62,12 +103,12 @@ export default function AddressList({
             />
           </div>
         ) : null}
-        {addressData && !isLoading
-          ? addressData.map((item: AddressObject) => {
+        {filteredData && !isLoading
+          ? filteredData.map((item: AddressObject) => {
               return (
                 <div
                   key={item.lmkKey}
-                  className="h-[45px] w-[100%] pl-[10px] font- logoFont flex items-center rounded-default cursor-pointer hover:bg-lightGreen/25"
+                  className="h-[45px] w-[100%] pl-[10px] font-logoFont flex items-center rounded-default cursor-pointer hover:bg-lightGreen/25"
                   onClick={(e: React.MouseEvent<HTMLDivElement>): void => {
                     selectionHandler(item.lmkKey);
                   }}
