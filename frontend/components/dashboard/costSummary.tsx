20a29df4aba5bf5ea91b734f98216e0c6f3f9172
Jarryd Cheso <jarryd.cheso@gmail.com>
2022-03-08 12:08:52 +0000

    refactor cost summary for mobile

--- frontend/components/dashboard/costSummary.tsx
+++ frontend/components/dashboard/costSummary.tsx
@@ -1,77 +1,123 @@
-import React, {Dispatch, SetStateAction} from 'react';
-import { GiWaterDrop, GiFireplace } from 'react-icons/gi';
-import { BsLightbulb } from 'react-icons/bs';
-import { epcCertificateObject, packagedAnalyticsObject } from '../../types';
-import {AiOutlineArrowRight} from 'react-icons/ai';
+import React, { Dispatch, SetStateAction } from "react";
+import { GiWaterDrop, GiFireplace } from "react-icons/gi";
+import { BsLightbulb } from "react-icons/bs";
+import { epcCertificateObject, packagedAnalyticsObject } from "../../types";
+import { AiOutlineArrowRight } from "react-icons/ai";
 
 interface props {
-  data: epcCertificateObject['House']['costs'];
-  analytics: packagedAnalyticsObject['cost'];
+  data: epcCertificateObject["House"]["costs"];
+  analytics: packagedAnalyticsObject["cost"];
   setModalHandler: Dispatch<SetStateAction<string>>;
 }
 
-export default function CostSummary({ data, analytics, setModalHandler }: props) {
+export default function CostSummary({
+  data,
+  analytics,
+  setModalHandler,
+}: props) {
   return (
-    <div className="py-2 h-full">
-      <div className="flex h-3/4">
-        <div className="w-1/3 h-full pl-1 flex flex-col gap-1">
-          <div className="w-full justify-center font-bold text-lg pt-1 flex items-center gap-2">
-            Heating <GiFireplace color="#eb6434" />{' '}
-          </div>
-
-          <div className="flex gap-2 flex-col h-full">
-            <div className="flex flex-col gap-2 w-full py-1 pb-3 border border-x-0 border-t-0 text-center">
-              <div>
-                Current Cost: <b>£{data.heatingCostCurrent}</b>/year
-              </div>
-              <div>
-                Potential Cost: <b>£{data.heatingCostPotential}</b>/year
-              </div>
+    <div className="py-2 flex flex-row h-5/6">
+      {/* HEATING COLUMN */}
+      <div className="w-1/3 flex flex-col">
+        <div className="justify-center font-bold text-base md:text-lg pt-1 flex items-center gap-2">
+          Heating <GiFireplace color="#eb6434" />
+        </div>
+        <div className="flex flex-col py-1 justify-between text-center h-full">
+          <div className="py-2">
+            <div className="text-sm md:text-base font-normal underline pb-1">
+              Current Cost
             </div>
-            <div className="text-center px-2">
-              Area Average: <i className="font-bold">£{analytics?.meanCurrentHeatingCost}/year</i>
+            <div>
+              <b>£{data.heatingCostCurrent}</b>/year
             </div>
           </div>
+          <div className="py-2">
+            <div className="text-sm md:text-base font-normal underline pb-1">
+              Potential Cost
+            </div>
+            <div>
+              <b>£{data.heatingCostPotential}</b>/year
+            </div>
+          </div>
 
+          <div className="border border-x-0 border-t-0"></div>
+          <div className="py-2">
+            <div className="text-sm md:text-base font-normal underline pb-1">
+              Area Average
+            </div>
+            <div>
+              <b>£{analytics?.meanCurrentHeatingCost}</b>/year
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="border border-r-1 border-l-0 border-y-0"></div>
+      {/* HOT WATER COLUMN */}
+      <div className="w-1/3 flex flex-col">
+        <div className="justify-center font-bold text-base md:text-lg pt-1 flex items-center gap-2">
+          Hot Water <GiWaterDrop color="#34bdeb" />{" "}
         </div>
 
-        <div className="w-1/3 border border-y-0 h-full pl-2 flex flex-col gap-1">
-          <div className="w-full justify-center font-bold text-lg pt-1 flex items-center gap-2">
-            Hot Water <GiWaterDrop color="#34bdeb" />{' '}
-          </div>
-
-          <div className="flex gap-2 flex-col h-full">
-            <div className="flex flex-col gap-2 w-full py-1 pb-3 border border-x-0 border-t-0 text-center">
-              <div>
-                Current Cost: <b>£{data.hotWaterCostCurrent}</b>/year
-              </div>
-              <div>
-                Potential Cost: <b>£{data.hotWaterCostPotential}</b>/year
-              </div>
+        <div className="flex flex-col py-1 justify-between text-center h-full">
+          <div className="py-2">
+            <div className="text-sm md:text-base font-normal underline pb-1">
+              Current Cost
             </div>
-            <div className="text-center px-2">
-              Area Average: <i className="font-bold">£{analytics?.meanCurrentHotWaterCost}/year</i>
+            <div>
+              <b>£{data.hotWaterCostCurrent}</b>/year
             </div>
           </div>
-
+          <div className="py-2">
+            <div className="text-sm md:text-base font-normal underline pb-1">
+              Potential Cost
+            </div>
+            <div>
+              <b>£{data.hotWaterCostPotential}</b>/year
+            </div>
+          </div>
+          <div className="border border-x-0 border-t-0"></div>
+          <div className="py-2">
+            <div className="text-sm md:text-base font-normal underline pb-1">
+              Area Average
+            </div>
+            <div>
+              <b>£{analytics?.meanCurrentHotWaterCost}</b>/year
+            </div>
+          </div>
         </div>
-        
-        <div className="w-1/3 h-full pl-1 flex flex-col gap-1">
-          <div className="w-full justify-center font-bold pt-1 flex text-lg items-center gap-2">
-            Lighting <BsLightbulb color="#d4c328" />{' '}
+      </div>
+      <div className="border border-r-1 border-l-0 border-y-0"></div>
+      {/* LIGHTING COLUMN */}
+      <div className="w-1/3 flex flex-col h-full">
+        <div className="justify-center font-bold text-base md:text-lg pt-1 flex items-center gap-2">
+          Lighting <BsLightbulb color="#d4c328" />
+        </div>
+
+        <div className="flex flex-col py-1 justify-between text-center h-full">
+          <div className="py-2">
+            <div className="text-sm md:text-base font-normal underline pb-1">
+              Current Cost
+            </div>
+            <div>
+              <b>£{data.lightingCostCurrent}</b>/year
+            </div>
+          </div>
+          <div className="py-2">
+            <div className="text-sm md:text-base font-normal underline pb-1">
+              Potential Cost
+            </div>
+            <div>
+              <b>£{data.lightingCostPotential}</b>/year
+            </div>
           </div>
 
-          <div className="flex gap-2 flex-col h-full">
-            <div className="flex flex-col gap-2 w-full py-1 pb-3 border border-x-0 border-t-0 text-center">
-              <div>
-                Current Cost: <b>£{data.lightingCostCurrent}</b>/year
-              </div>
-              <div>
-                Potential Cost: <b>£{data.lightingCostPotential}</b>/year
-              </div>
+          <div className="border border-x-0 border-t-0"></div>
+          <div className="py-2">
+            <div className="text-sm md:text-base font-normal underline pb-1">
+              Area Average
             </div>
-            <div className="text-center px-2">
-              Area Average:<i className="font-bold">£{analytics?.meanCurrentLightingCost}/year</i> 
+            <div>
+              <b>£{analytics?.meanCurrentLightingCost}</b>/year
             </div>
           </div>
         </div>
