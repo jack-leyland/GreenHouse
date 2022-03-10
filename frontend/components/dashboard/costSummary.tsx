1f5c94e63473af48fc612b70ecff5a093175f96d
Will Thomson <will.thomson4@gmail.com>
2022-03-10 14:48:50 +0000

    refactor cost card

--- frontend/components/dashboard/costSummary.tsx
+++ frontend/components/dashboard/costSummary.tsx
@@ -2,7 +2,7 @@ import React, { Dispatch, SetStateAction } from "react";
 import { GiWaterDrop, GiFireplace } from "react-icons/gi";
 import { BsLightbulb } from "react-icons/bs";
 import { epcCertificateObject, packagedAnalyticsObject } from "../../types";
-import { AiOutlineArrowRight } from "react-icons/ai";
+import { AiFillQuestionCircle } from "react-icons/ai";
 
 interface props {
   data: epcCertificateObject["House"]["costs"];
@@ -15,113 +15,82 @@ export default function CostSummary({
   analytics,
   setModalHandler,
 }: props) {
+
+  const tdStyle = "border ";
+  const titleStyle = "justify-center font-semibold text-base md:text-lg flex items-center gap-1"
+  const headStyle = tdStyle + "font-semibold md:text-lg"
+
   return (
-    <div className="py-2 flex flex-row h-5/6">
-      {/* HEATING COLUMN */}
-      <div className="w-1/3 flex flex-col">
-        <div className="justify-center font-bold text-base md:text-lg pt-1 flex items-center gap-2">
-          Heating <GiFireplace color="#eb6434" />
-        </div>
-        <div className="flex flex-col py-1 justify-between text-center h-full">
-          <div className="py-2">
-            <div className="text-sm md:text-base font-normal underline pb-1">
-              Current Cost
-            </div>
-            <div>
-              <b>£{data.heatingCostCurrent}</b>/year
-            </div>
-          </div>
-          <div className="py-2">
-            <div className="text-sm md:text-base font-normal underline pb-1">
-              Potential Cost
-            </div>
-            <div>
-              <b>£{data.heatingCostPotential}</b>/year
-            </div>
-          </div>
+    <div className="py-2 flex flex-col h-5/6">
 
-          <div className="border border-x-0 border-t-0"></div>
-          <div className="py-2">
-            <div className="text-sm md:text-base font-normal underline pb-1">
-              Area Average
-            </div>
-            <div>
-              <b>£{analytics?.meanCurrentHeatingCost}</b>/year
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="border border-r-1 border-l-0 border-y-0"></div>
-      {/* HOT WATER COLUMN */}
-      <div className="w-1/3 flex flex-col">
-        <div className="justify-center font-bold text-base md:text-lg pt-1 flex items-center gap-2">
-          Hot Water <GiWaterDrop color="#34bdeb" />{" "}
-        </div>
+    <table className="h-full text-center">
+      <tr className="bg-gray-50">
+      <td className="bg-white flex justify-center items-center h-full">
+        <AiFillQuestionCircle
+            size={24}
+            className="hover:cursor-pointer"
+            onClick={() => setModalHandler("costs")}
+          />  
+      </td>
+      <td className={headStyle}>Current Cost</td>
+      <td className={headStyle}>Potential Cost</td>
+      <td className={headStyle}>Area Average</td>
+      </tr>
 
-        <div className="flex flex-col py-1 justify-between text-center h-full">
-          <div className="py-2">
-            <div className="text-sm md:text-base font-normal underline pb-1">
-              Current Cost
+      <tbody>
+        <tr>
+          <td className={tdStyle}>
+            <div className={titleStyle}>
+              Heating <GiFireplace color="#eb6434" />
             </div>
-            <div>
-              <b>£{data.hotWaterCostCurrent}</b>/year
-            </div>
-          </div>
-          <div className="py-2">
-            <div className="text-sm md:text-base font-normal underline pb-1">
-              Potential Cost
-            </div>
-            <div>
-              <b>£{data.hotWaterCostPotential}</b>/year
-            </div>
-          </div>
-          <div className="border border-x-0 border-t-0"></div>
-          <div className="py-2">
-            <div className="text-sm md:text-base font-normal underline pb-1">
-              Area Average
-            </div>
-            <div>
-              <b>£{analytics?.meanCurrentHotWaterCost}</b>/year
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="border border-r-1 border-l-0 border-y-0"></div>
-      {/* LIGHTING COLUMN */}
-      <div className="w-1/3 flex flex-col h-full">
-        <div className="justify-center font-bold text-base md:text-lg pt-1 flex items-center gap-2">
-          Lighting <BsLightbulb color="#d4c328" />
-        </div>
+          </td>
+          <td className={tdStyle}>
+          <b>£{data.heatingCostCurrent}</b>/year
+          </td>
+          <td className={tdStyle}>
+          <b>£{data.heatingCostPotential}</b>/year
+          </td>
+          <td className={tdStyle}>
+          <b>£{analytics?.meanCurrentHeatingCost}</b>/year
+          </td>
+        </tr>
 
-        <div className="flex flex-col py-1 justify-between text-center h-full">
-          <div className="py-2">
-            <div className="text-sm md:text-base font-normal underline pb-1">
-              Current Cost
+        <tr className="bg-gray-50">
+          <td className={tdStyle}>
+            <div className={titleStyle}>
+              Hot Water <GiWaterDrop color="#34bdeb" />{" "}
             </div>
-            <div>
-              <b>£{data.lightingCostCurrent}</b>/year
-            </div>
-          </div>
-          <div className="py-2">
-            <div className="text-sm md:text-base font-normal underline pb-1">
-              Potential Cost
-            </div>
-            <div>
-              <b>£{data.lightingCostPotential}</b>/year
-            </div>
-          </div>
+          </td>
+          <td className={tdStyle}>
+          <b>£{data.hotWaterCostCurrent}</b>/year
+          </td>
+          <td className={tdStyle}>
+          <b>£{data.hotWaterCostPotential}</b>/year
+          </td>
+          <td className={tdStyle}>
+          <b>£{analytics?.meanCurrentHotWaterCost}</b>/year
+          </td>
+        </tr>
 
-          <div className="border border-x-0 border-t-0"></div>
-          <div className="py-2">
-            <div className="text-sm md:text-base font-normal underline pb-1">
-              Area Average
+        <tr>
+          <td className={tdStyle}>
+            <div className={titleStyle}>
+              Lighting <BsLightbulb color="#d4c328" />
             </div>
-            <div>
-              <b>£{analytics?.meanCurrentLightingCost}</b>/year
-            </div>
-          </div>
-        </div>
-      </div>
+          </td>
+          <td className={tdStyle}>
+          <b>£{data.lightingCostCurrent}</b>/year
+          </td>
+          <td className={tdStyle}>
+          <b>£{data.lightingCostPotential}</b>/year
+          </td>
+          <td className={tdStyle}>
+          <b>£{analytics?.meanCurrentLightingCost}</b>/year
+          </td>
+        </tr>
+        </tbody>
+    </table>
+
     </div>
   );
 }
