45d3c25839a7286aa4ce3a11f6194b0a52cab8c2
Joshua Cheng <joshcheng4@gmail.com>
2022-05-15 14:35:31 +0100

    Add graphiql view based on ENV

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -17,12 +17,15 @@ from django.contrib import admin
 from django.urls import path
 from graphene_django.views import GraphQLView
 from .views import ping
+import os
 
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("ping/", ping, name="ping"),
-    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
-    # graphiql= os.environ.get("ENV") == "DEV"
+    path(
+        "graphql",
+        csrf_exempt(GraphQLView.as_view(graphiql=os.environ.get("ENV") == "DEV")),
+    ),
 ]
