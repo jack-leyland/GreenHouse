8d522122992c2207b07de137aca7ab5b6e6ebca2
Joshua Cheng <joshcheng4@gmail.com>
2022-05-16 11:54:18 +0100

    Add tests for certificate and recommendations queries

--- backend/app/tests.py
+++ backend/app/tests.py
@@ -22,3 +22,112 @@ class QueryTestCase(GraphQLTestCase):
 
         content = json.loads(response.content)
         self.assertResponseNoErrors(response)
+
+    def test_certificate_query(self):
+        response = self.query(
+            """
+            query certificate($lmk: String!) {
+                certificate(lmk: $lmk) {
+                    address
+                    address2
+                    address3
+                    localAuthorityLabel
+                    posttown
+                    county
+                    currentEnergyRating
+                    postcode
+                    potentialEnergyRating
+                    co2EmissionsCurrent
+                    co2EmissionsPotential
+                    co2EmissCurrPerFloorArea
+                    propertyType
+                    builtForm
+                    constructionAgeBand
+                    totalFloorArea
+                    energyTariff
+                    environmentImpactPotential
+                    environmentImpactCurrent
+                    energyConsumptionPotential
+                    energyConsumptionCurrent
+                    roofDescription
+                    roofEnergyEff
+                    roofEnvEff
+                    windowsDescription
+                    windowsEnvEff
+                    windowsEnergyEff
+                    glazedType
+                    glazedArea
+                    multiGlazeProportion
+                    mainsGasFlag
+                    numberHeatedRooms
+                    heatLossCorridor
+                    unheatedCorridorLength
+                    mainheatDescription
+                    mainheatEnvEff
+                    mainheatEnergyEff
+                    mainFuel
+                    mainheatcontDescription
+                    mainheatcEnergyEff
+                    mainheatcEnvEff
+                    secondheatDescription
+                    sheatingEnergyEff
+                    lowEnergyLighting
+                    lightingEnergyEff
+                    lightingEnvEff
+                    wallsDescription
+                    wallsEnergyEff
+                    wallsEnvEff
+                    hotwaterDescription
+                    hotWaterEnvEff
+                    hotWaterEnergyEff
+                    floorDescription
+                    floorEnergyEff
+                    floorEnvEff
+                    photoSupply
+                    solarWaterHeatingFlag
+                    mechanicalVentilation
+                    floorLevel
+                    flatStoreyCount
+                    flatTopStorey
+                    inspectionDate
+                    heatingCostPotential
+                    hotWaterCostPotential
+                    heatingCostCurrent
+                    hotWaterCostCurrent
+                    lightingCostPotential
+                    lightingCostCurrent
+                    currentEnergyEfficiency
+                    potentialEnergyEfficiency
+                }
+            }
+            """,
+            variables={
+                "lmk": "03caacf3e712ce8428682340e06907fab1ed751090446397dc714a2fa3b4d571"
+            },
+        )
+
+        content = json.loads(response.content)
+        self.assertResponseNoErrors(response)
+
+    def test_recommendations_query(self):
+        response = self.query(
+            """
+            query recommendations($lmk: String!) {
+                recommendations(lmk: $lmk) {
+                    date
+                    postcode
+                    improvementId
+                    improvementIdText
+                    cost
+                    completed
+                    indicativeCost
+                }
+            }
+            """,
+            variables={
+                "lmk": "03caacf3e712ce8428682340e06907fab1ed751090446397dc714a2fa3b4d571"
+            },
+        )
+
+        content = json.loads(response.content)
+        self.assertResponseNoErrors(response)
